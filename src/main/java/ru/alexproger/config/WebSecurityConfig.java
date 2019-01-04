df62c6f580b10ae9d1e26d2c03ed519106ff4777
Gorunov Alexander <mailto:gorunov.sasha85@gmail.com>
2019-01-04 12:12:50 +0300

    Добавление страницы управления правами доступа.

--- src/main/java/ru/alexproger/config/WebSecurityConfig.java
+++ src/main/java/ru/alexproger/config/WebSecurityConfig.java
@@ -3,18 +3,20 @@ package ru.alexproger.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import ru.alexproger.service.UserSevice;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private UserDetailsService service;
+    private UserSevice service;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
